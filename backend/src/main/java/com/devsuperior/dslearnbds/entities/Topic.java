8dfc71eb3455c48f2eaacf0e85db4aaa26ee84e5
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2021-08-12 16:42:07 -0400

    Entity Reply implemented

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Topic.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Topic.java
@@ -2,7 +2,9 @@ package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -15,6 +17,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -46,11 +49,18 @@ public class Topic implements Serializable {
 	@JoinColumn(name = "lesson_id")
 	private Lesson lesson;
 	
+	@ManyToOne
+	@JoinColumn(name = "reply_id")
+	private Reply answer;
+	
 	@ManyToMany
 	@JoinTable(name = "tb_topic_likes", 
 	  joinColumns = @JoinColumn(name = "topic_id"),
 	  inverseJoinColumns = @JoinColumn(name = "user_id"))
 	private Set<User> likes = new HashSet<>();
+	
+	@OneToMany(mappedBy = "topic")
+	private List<Reply> replies = new ArrayList<>();
 
 	public Topic() { }
 	
@@ -129,6 +139,18 @@ public class Topic implements Serializable {
 	
 	public Set<User> getLikes() {
 		return likes;
+	}
+	
+	public Reply getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(Reply answer) {
+		this.answer = answer;
+	}
+
+	public List<Reply> getReplies() {
+		return replies;
 	}
 
 	@Override
